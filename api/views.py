ae73346b18efed8d9101882e7044a67f98941c6d
atroopa <atroopa@gmail.com>
2023-09-23 12:56:16 +0330

    api serializer work true

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from blog.models import Article
+from .serializers import ArticleSerializer
 
 # Create your views here.
+class ArticleListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
