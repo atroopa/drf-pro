0add5ec1783576006f39b6659a9f739166ce9b1e
atroopa <atroopa@gmail.com>
2023-09-23 17:29:21 +0330

    final article

--- api/views.py
+++ api/views.py
@@ -1,13 +1,14 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAdminUser
 from blog.models import Article
 from .serializers import ArticleSerializer, UserSerializer
 from django.contrib.auth.models import User
+from .permissions import IsSuperUser, IsAuthorOReadOnly, IsStaffOrReadonly
 
 # Create your views here.
 class ArticleListView(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = (IsStaffOrReadonly, IsAuthorOReadOnly)
 
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
@@ -16,9 +17,9 @@ class ArticleDetail(RetrieveUpdateDestroyAPIView):
 class UserListView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsSuperUser,)
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsSuperUser,)
