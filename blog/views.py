371cc0d1bd2935af299184a1433e217319aa2ec3
atroopa <atroopa@gmail.com>
2023-09-23 12:13:13 +0330

    start app api for drf

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
-from typing import Any
 from django.views.generic import ListView
 from .models import Article
 # Create your views here.
 
 class ArticleList(ListView):
-    def get_queryset(self) :
+
+    def get_queryset(self):
         return Article.objects.filter(status=True)
