448e694c80f97d970a0552c4d6277b3b76c90916
atroopa <atroopa@gmail.com>
2023-09-23 14:58:58 +0330

    add user model to serializer and views

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path , include
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetail, UserListView, UserDetail
 
 app_name = "api"
 
 urlpatterns = [
-    path("", ArticleListView.as_view(), name="list")
+    path("", ArticleListView.as_view(), name="list"),
+    path("<int:pk>", ArticleDetail.as_view(), name="detail"),
+    path("users", UserListView.as_view(), name="user_list"),
+    path("users/<int:pk>", UserDetail.as_view(), name="user_detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,21 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from blog.models import Article
-from .serializers import ArticleSerializer
+from .serializers import ArticleSerializer, UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
-class ArticleListView(ListAPIView):
+class ArticleListView(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
+class ArticleDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class UserListView(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
