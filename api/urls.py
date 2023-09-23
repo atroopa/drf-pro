8152faeb179bd8ec1f8cd72d4bf4f781db01df65
atroopa <atroopa@gmail.com>
2023-09-23 15:27:17 +0330

    make permissions files

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ app_name = "api"
 urlpatterns = [
     path("", ArticleListView.as_view(), name="list"),
     path("<int:pk>", ArticleDetail.as_view(), name="detail"),
-    path("users", UserListView.as_view(), name="user_list"),
+    path("users/", UserListView.as_view(), name="user_list"),
     path("users/<int:pk>", UserDetail.as_view(), name="user_detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAdminUser
 from blog.models import Article
 from .serializers import ArticleSerializer, UserSerializer
 from django.contrib.auth.models import User
@@ -15,7 +16,9 @@ class ArticleDetail(RetrieveUpdateDestroyAPIView):
 class UserListView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
